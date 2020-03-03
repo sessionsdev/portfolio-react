c5200b691be7cd16bdd8056d7a4357b8c4b35448
Jon Sessions <jon@sessionsdev.com>
2020-03-02 20:43:08 -0500

    Updates

---
+++ precache-manifest.2679a06c0f54999795a387047dc99548.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce85d879cf7babbfa523bfe794b2ce50",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "fc0a0ae42d67e85cc20b",
+    "url": "/portfolio-react/static/css/main.b18137ba.chunk.css"
+  },
+  {
+    "revision": "1c8922225d6084e000fa",
+    "url": "/portfolio-react/static/js/2.a65d38f1.chunk.js"
+  },
+  {
+    "revision": "8fc78b8514f9133162597e0ac4930dd3",
+    "url": "/portfolio-react/static/js/2.a65d38f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0a0ae42d67e85cc20b",
+    "url": "/portfolio-react/static/js/main.3403d250.chunk.js"
+  },
+  {
+    "revision": "72a8c294232039c6d319",
+    "url": "/portfolio-react/static/js/runtime-main.69a8648f.js"
+  },
+  {
+    "revision": "cc669a05ad313f368b6ee1cc4aa2d87f",
+    "url": "/portfolio-react/static/media/about.cc669a05.png"
+  },
+  {
+    "revision": "a7c80c00ad827b2bc0b8bd47cbf0545f",
+    "url": "/portfolio-react/static/media/chirpblog_homepage.a7c80c00.jpg"
+  },
+  {
+    "revision": "a914004ee9e51aaea6ceb8b53f55546b",
+    "url": "/portfolio-react/static/media/codingnomads.a914004e.png"
+  },
+  {
+    "revision": "131f34d34e0ad09b78858cc0e4a7efcf",
+    "url": "/portfolio-react/static/media/header.131f34d3.png"
+  },
+  {
+    "revision": "242940a7b6d8d774e089d074c47f1b77",
+    "url": "/portfolio-react/static/media/itm.242940a7.png"
+  },
+  {
+    "revision": "41c497e9c4357ee1e8ca48957b056a0b",
+    "url": "/portfolio-react/static/media/userauth.41c497e9.png"
+  }
+]);
